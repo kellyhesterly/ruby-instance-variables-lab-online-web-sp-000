ee773dd9e442e5b272bf260a6ed4c523b7c8e530
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-04 17:35:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,11 @@
 class Dog
 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
-
-  def name
-    @this_dogs_name
-  end
+def name=(dog_name)
+  @this_dogs_name = dog_name
 end
 
-lassie = Dog.new
-lassie.name = "Lassie"
+def name
+  @this_dogs_name
+end
 
-puts lassie.name
+end
